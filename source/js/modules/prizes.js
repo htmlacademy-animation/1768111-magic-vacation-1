2f2fd7556200dae70859dc12e02e85c12d9392a2
VitaliyPliss <v.pliss@mail.ru>
2022-08-14 23:02:26 +0300

    сделал запуск анимации единожды

--- source/js/modules/prizes.js
+++ source/js/modules/prizes.js
@@ -1,11 +1,15 @@
 export default () => {
-  let svgAnimations = document.querySelectorAll(`.svg-animation`);
-  let menu = document.querySelector(`.page-header__menu`);
+  const prizesLink = document.querySelector(`.js-menu-link[data-href="prizes"]`);
+  const svgAnimations = document.querySelectorAll(`.svg-animation`);
 
-  // перезапуск svg анимаций при смене страниц
-  menu.addEventListener(`click`, () => {
+  const restartAnimation = () => {
     svgAnimations.forEach((animation) => {
       animation.src = animation.src;
     });
-  });
+    prizesLink.removeEventListener(`click`, restartAnimation);
+  };
+
+  if (window.location.hash !== `#prizes`) {
+    prizesLink.addEventListener(`click`, restartAnimation);
+  }
 };
