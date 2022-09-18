8b1caacd9bc3b1578a8570644174146a773f1b76
VitaliyPliss <v.pliss@mail.ru>
2022-09-18 17:39:59 +0300

    правки после ревью

--- source/js/modules/game.js
+++ source/js/modules/game.js
@@ -3,11 +3,12 @@ export default () => {
   const timer = screen.querySelectorAll(`.game__counter span`);
   const minutes = timer[0];
   const seconds = timer[1];
+  let currentSecond = null;
   let frame;
 
   const finish = Date.now() + 300000; // 5 minutes
 
-  const drawTaimer = () => {
+  const drawTimer = () => {
     const timeLeft = finish - Date.now();
 
     if (timeLeft < 1) {
@@ -15,19 +16,29 @@ export default () => {
       return;
     }
 
-    minutes.textContent = (`` + Math.floor(timeLeft / 1000 / 60) % 60).length === 1 ?
-      `0` + Math.floor(timeLeft / 1000 / 60) % 60 :
-      Math.floor(timeLeft / 1000 / 60) % 60;
-    seconds.textContent = (`` + Math.floor(timeLeft / 1000) % 60).length === 1 ?
-      `0` + Math.floor(timeLeft / 1000) % 60 :
-      Math.floor(timeLeft / 1000) % 60;
+    const minutesLeft = Math.floor(timeLeft / 1000 / 60) % 60;
+    const secondsLeft = Math.floor(timeLeft / 1000) % 60;
+
+    minutes.textContent = (`` + minutesLeft).length === 1 ?
+      `0` + minutesLeft :
+      minutesLeft;
+    seconds.textContent = (`` + secondsLeft).length === 1 ?
+      `0` + secondsLeft :
+      secondsLeft;
 
     if (screen.classList.contains(`active`)) {
-      frame = requestAnimationFrame(drawTaimer);
+      if (currentSecond !== secondsLeft) {
+        currentSecond = secondsLeft;
+        frame = requestAnimationFrame(drawTimer);
+      } else {
+        setTimeout(() => {
+          frame = requestAnimationFrame(drawTimer);
+        }, 1000);
+      }
     } else {
       cancelAnimationFrame(frame);
     }
   };
 
-  frame = requestAnimationFrame(drawTaimer);
+  frame = requestAnimationFrame(drawTimer);
 };
